2780ecdb65d7b91057b06154b95f72f64eff8dee
jbairoso <jbairoso@gmail.com>
2022-05-24 17:58:51 -0700

    PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { animals } = require(`./data/animals.json`);
 const express = require("express");
 const { type } = require("express/lib/response");
-const PORT = process.env.PORT|| 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
