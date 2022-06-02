849839b5af717027c0f6f36bc647680c687d8690
jbairoso <jbairoso@gmail.com>
2022-06-01 21:13:27 -0700

    install additional html files and js files

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const path = require("path");
 const express = require("express");
 const { animals } = require(`./data/animals.json`);
+const req = require("express/lib/request");
+const { ppid } = require("process");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -9,6 +11,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -107,6 +111,22 @@ app.post("/api/animals", (req, res) => {
     const animal = createNewAnimal(req.body, animals);
     res.json(animal);
   }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req,res) =>{
+  res.sendFile(path.join(__dirname,'./public/animals.html'));
+});
+
+app.get('/zookeepers', (req,res) => {
+  res.sendFile(path.join(__dirname,'./public/zookeepers.html'));
+});
+
+app.get('*', (req, res) =>{
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
